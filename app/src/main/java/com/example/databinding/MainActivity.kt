8625afb4d9a2fb09dce3b2b4082efb48bacdc252
Narendra Teja <narendrat@stgit.com>
2022-07-07 17:04:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/databinding/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.databinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.student = getStudent()
+    }
+
+    private fun getStudent(): Student {
+        return Student(1, "Teja Narendra", "tejanarendra@gmail.com")
+    }
+}
---
+++ app/src/main/java/com/example/databinding/Student.kt
@@ -0,0 +1,7 @@
+package com.example.databinding
+
+data class Student(
+    var id: Int,
+    var name: String,
+    var email: String
+)
